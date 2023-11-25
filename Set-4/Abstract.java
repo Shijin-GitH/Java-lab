b33817f741929e41fe2feb6e53d396bfca1b7033
Shijin Abraham <115648866+Shijin-GitH@users.noreply.github.com>
2023-11-25 13:25:48 +0530

    Update Abstract.java

--- Set-4/Abstract.java
+++ Set-4/Abstract.java
@@ -1,36 +1,49 @@
+import java.util.Scanner;
+
 abstract class Shape {
     abstract void numberOfSides();
 }
 
 class Rectangle extends Shape {
-    @Override
     void numberOfSides() {
         System.out.println("A rectangle has 4 sides.");
     }
 }
 
 class Triangle extends Shape {
-    @Override
     void numberOfSides() {
         System.out.println("A triangle has 3 sides.");
     }
 }
 
 class Hexagon extends Shape {
-    @Override
     void numberOfSides() {
         System.out.println("A hexagon has 6 sides.");
     }
 }
 
-public class Abstract {
+public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
-        Triangle triangle = new Triangle();
-        Hexagon hexagon = new Hexagon();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a shape (rectangle, triangle, hexagon):");
+        String shapeType = scanner.nextLine().toLowerCase();
 
-        rectangle.numberOfSides();
-        triangle.numberOfSides();
-        hexagon.numberOfSides();
+        Shape shape;
+        switch (shapeType) {
+            case "rectangle":
+                shape = new Rectangle();
+                break;
+            case "triangle":
+                shape = new Triangle();
+                break;
+            case "hexagon":
+                shape = new Hexagon();
+                break;
+            default:
+                System.out.println("Invalid shape!");
+                return;
+        }
+
+        shape.numberOfSides();
     }
 }
